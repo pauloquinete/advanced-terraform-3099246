02db7f98898f2f1830d0ca81ca7373c0bf30ff9d
pauloquinete <43891466+pauloquinete@users.noreply.github.com>
2025-02-17 20:00:44 -0300

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -1,4 +1,8 @@
 ### NETWORK
+data "google_compute_network" "default" {
+  name                    = "default"
+  project                 = var.project-id
+}
 
 ## SUBNET
 resource "google_compute_subnetwork" "subnet-1" {
